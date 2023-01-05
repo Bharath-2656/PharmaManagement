87b3af2dfdf06aa9d148f01f39ec2d9b72ede933
Bharath <bharath2000madhu@gmail.com>
2023-01-05 08:34:01 +0530

    Initial Commit

---
+++ pharmaManagement/Modals/Medicine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace pharmaManagement.Modals
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string quantity { get; set; }
+        public string batchId { get; set; }
+        public int price { get; set; }
+        public DateTime expiryDate { get; set; }
+    }
+
+}
+
---
+++ pharmaManagement/Modals/MedicineDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pharmaManagement.Modals;
+
+namespace FirstWebApplication.Controllers
+{
+    public class MedicineDBContext : DbContext
+    {
+        public MedicineDBContext(DbContextOptions<MedicineDBContext> options) : base(options)
+        {
+
+        }
+
+        //Create Table here
+        public DbSet<Medicine> Users { get; set; }
+    }
+}
