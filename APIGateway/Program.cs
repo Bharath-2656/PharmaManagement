86119940da7f278fe6ad06f94bcb8b8ddd09668c
Bharath <bharath2000madhu@gmail.com>
2023-01-19 14:43:47 +0530

    Added API GAteway

---
+++ APIGateway/Program.cs
@@ -0,0 +1,37 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("ocelot.json")
+                            .Build();
+
+// Add services to the container.
+builder.Services.AddRazorPages(); 
+builder.Services.AddOcelot(configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseOcelot();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
